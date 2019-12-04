9a2064aa84bff1c3a93704d5a839de92d77f096c
dmitrii <ketovdk@Rambler.ru>
2019-12-04 21:25:06 +0500

    mens api created
    getAll, getid, put

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
-app= Flask(__name__)
+from models import db, Men
+from routes import api
 
-@app.route('/api/hello')
-def hello():
-    return "Hello world"
+app= Flask(__name__)
+app.register_blueprint(api)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    db.session.add(Men(name = 'Bob'))
+    db.session.add(Men(name = 'Rocky'))
+    db.session.commit()
+
 
 if __name__ == "__main__":
     app.run()
